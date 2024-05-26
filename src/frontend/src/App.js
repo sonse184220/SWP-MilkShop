3fc66f58364109a8b6879d5c8249ec6f8379441d
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-05-26 17:52:24 +0700

    App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
 import Test from './Test-GetSearchAPI.js';
 
 function App() {
   return (
+    <>    
     <div>
       <div><Header /></div>
       <br />
       <div><Test /></div>
+      <br />
+      <div><Footer /></div>      
     </div>
+    
+    </>
+
   );
 }
 
