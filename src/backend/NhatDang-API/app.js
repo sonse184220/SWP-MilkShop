419f607da4c05f5d73b9757001da37f3319b2a7e
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-05-28 20:09:58 +0700

    Update thêm View chi tiết product
    Cách kiểm tra : http://localhost:4500/product/P001

--- src/backend/NhatDang-API/app.js
+++ src/backend/NhatDang-API/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/authRoutes');
+const productRoutes = require('./src/routes/productRoutes');
 
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 4500;
 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
+app.use('/product', productRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
