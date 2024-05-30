bd1d1775c1aa698c1cb898cea2ae3c5da9578a22
PTPhong-PPL <phongptse183388@fpt.edu.vn>
2024-05-30 14:26:56 +0700

    Gộp src backend

--- src/backend/src/utils/dbConnection.js
+++ src/backend/src/utils/dbConnection.js
@@ -4,6 +4,11 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('PORT:', process.env.PORT);
 
 const port = process.env.PORT || 3000
 
