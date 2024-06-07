de152f766668333740177a1c091706e9eacab509
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-06-07 23:13:43 +0700

    Update AllBlog.jsx

--- src/frontend/src/components/Blog/AllBlog.jsx
+++ src/frontend/src/components/Blog/AllBlog.jsx
@@ -97,6 +97,9 @@ const AllBlog = () => {
         <div className="row">
           <div className="col-lg-8">
             <h3>All Blogs</h3>
+            <div className='searchBlog'>Search blog: <input type='text' placeholder='Search blog'/>
+                <button>Search</button>
+            </div>
             {blogs.map((blog, index) => (
               <BlogPost
                 key={index}
