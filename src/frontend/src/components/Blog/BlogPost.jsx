e645d20c4e912433477632eb6e283f2652650470
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-06-15 09:57:20 +0700

    Update BlogPost.jsx

--- src/frontend/src/components/Blog/BlogPost.jsx
+++ src/frontend/src/components/Blog/BlogPost.jsx
@@ -2,28 +2,21 @@ import React from 'react';
 import './BlogPost.css';
 import ProductList from '../Product/ProductList';
 
-const BlogPost = ({ title, author, content, date, image, products }) => {
+const BlogPost = ({  blogs = []  }) => {
   return (
-    <div className="blog-post">
-      <div className="blog-post-header">
-        <h2>{title}</h2>
-        <div className="blog-post-author">
-          <img src={author.image} alt={author.name} />
-          <div>
-            <h3>{author.name}</h3>
-            <p>{author.bio}</p>
+    <>
+      {blogs.map((blog) => (
+        <div className="blog-post">
+          <div className="blog-post-header">
+            <h2>{blog.Name}</h2>
+          </div>
+          <div className="blog-post-content">
+            <img src={`/img/${blog.BlogID}.png`} />
+            <p>{blog.Content}</p>
           </div>
         </div>
-      </div>
-      <div className="blog-post-content">
-        {<img src={image} alt={title} />}
-        <p>{content}</p>
-      </div>
-      <div className="blog-post-footer">
-        <p>Published on {date.toLocaleDateString()}</p>
-      </div>
-      {products && <ProductList products={products} />}
-    </div>
+      ))}
+    </>
   );
 };
 
