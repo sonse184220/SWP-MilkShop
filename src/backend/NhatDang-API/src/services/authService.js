fafd262495dacf42f4d089f8b7f301c373623513
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-06-17 22:45:26 +0700

    Sửa bug phần xác nhận mail đăng kí bị đứng userID.
    Trước khi test, nhớ chạy query tui gửi trên zalo nha.

--- src/backend/NhatDang-API/src/services/authService.js
+++ src/backend/NhatDang-API/src/services/authService.js
@@ -23,39 +23,31 @@ passport.use(new GoogleStrategy({
             return done(null, results[0]);
         }
 
-        const getMaxUserIdQuery = "SELECT MAX(CAST(SUBSTR(UserID, 3) AS UNSIGNED)) AS maxUserId FROM MEMBER WHERE UserID LIKE 'UG%'";
-        connection.query(getMaxUserIdQuery, (err, results) => {
+        const newUser = {
+            Name: profile.displayName || '',
+            Email: email,
+            Phone: '',
+            Address: '',
+            RewardPoints: 0,
+            Verified: 1
+        };
+
+        const query = 'INSERT INTO TEMP_MEMBER (Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?)';
+        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        connection.query(query, [newUser.Name, newUser.Email, newUser.Phone, newUser.Address, token], (err, result) => {
             if (err) return done(err);
-
-            const newUserId = `UG${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
-
-            const newUser = {
-                UserID: newUserId,
-                Name: profile.displayName || '',
-                Email: email,
-                Phone: '',
-                Address: '',
-                RewardPoints: 0,
-                Verified: 1
-            };
-
-            const query = 'INSERT INTO TEMP_MEMBER (UserID, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?)';
-            const token = jwt.sign({ userId: newUserId }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-            connection.query(query, [newUserId, newUser.Name, newUser.Email, newUser.Phone, newUser.Address, token], (err, result) => {
-                if (err) return done(err);
-                return done(null, newUser);
-            });
+            return done(null, { ...newUser, TempMemberID: result.insertId });
         });
     });
 }));
 
 passport.serializeUser((user, done) => {
-    done(null, user.UserID);
+    done(null, user.TempMemberID);
 });
 
 passport.deserializeUser((id, done) => {
-    const query = 'SELECT * FROM MEMBER WHERE UserID = ?';
+    const query = 'SELECT * FROM TEMP_MEMBER WHERE TempMemberID = ?';
     connection.query(query, [id], (err, results) => {
         if (err) return done(err);
         done(null, results[0]);
@@ -65,37 +57,32 @@ passport.deserializeUser((id, done) => {
 const registerUser = (userData, req, callback) => {
     const { Password, Name, Email, Phone, Address } = userData;
 
-    const checkQuery = 'SELECT * FROM MEMBER WHERE Email = ? OR Phone = ?';
-    connection.query(checkQuery, [Email, Phone], (err, results) => {
+    const checkQuery = 'SELECT * FROM MEMBER WHERE Email = ?';
+    connection.query(checkQuery, [Email], (err, results) => {
         if (err) return callback(err);
 
         if (results.length > 0) {
-            const existingUser = results[0];
-            let errorMessage = 'User already exists with ';
-            if (existingUser.Email === Email) {
-                errorMessage += 'this Email';
-            } else if (existingUser.Phone === Phone) {
-                errorMessage += 'this Phone number';
-            }
-            return callback(null, { message: errorMessage, status: 400 });
+            return callback(null, { message: 'User already exists with this Email', status: 400 });
         }
 
-        bcrypt.hash(Password, 10, (err, hashedPassword) => {
+        const tempCheckQuery = 'SELECT * FROM TEMP_MEMBER WHERE Email = ?';
+        connection.query(tempCheckQuery, [Email], (err, results) => {
             if (err) return callback(err);
 
-            const getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTR(UserID, 2) AS UNSIGNED)) AS maxUserId FROM MEMBER WHERE UserID LIKE "U%"';
-            connection.query(getMaxUserIdQuery, (err, results) => {
+            if (results.length > 0) {
+                return callback(null, { message: 'A verification email has already been sent to this Email.', status: 400 });
+            }
+
+            bcrypt.hash(Password, 10, (err, hashedPassword) => {
                 if (err) return callback(err);
 
-                const newUserId = `U${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
+                const token = jwt.sign({ email: Email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                const query = 'INSERT INTO TEMP_MEMBER (Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?)';
 
-                const token = jwt.sign({ userId: newUserId }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                const query = 'INSERT INTO TEMP_MEMBER (UserID, Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?, ?)';
-
-                connection.query(query, [newUserId, hashedPassword, Name, Email, Phone, Address, token], (err, result) => {
+                connection.query(query, [hashedPassword, Name, Email, Phone, Address, token], (err, result) => {
                     if (err) return callback(err);
 
-                    sendVerificationEmail(Email, token, newUserId, Phone, req)
+                    sendVerificationEmail(Email, token, result.insertId, Phone, req)
                         .then(() => {
                             console.log('Verification email sent to:', Email);
                             callback(null, { message: 'User registered successfully. Verification email sent.' });
@@ -140,7 +127,7 @@ const loginUser = (userData, callback) => {
 const verifyEmail = (token, callback) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const userId = decoded.userId;
+        const email = decoded.email;
 
         const selectQuery = 'SELECT * FROM TEMP_MEMBER WHERE Token = ?';
         connection.query(selectQuery, [token], (err, results) => {
@@ -148,15 +135,22 @@ const verifyEmail = (token, callback) => {
             if (results.length === 0) return callback(null, { message: 'Invalid or expired token', status: 400 });
 
             const user = results[0];
-            const insertQuery = 'INSERT INTO MEMBER (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified) VALUES (?, ?, ?, ?, ?, ?, 0, 1)';
-            const deleteQuery = 'DELETE FROM TEMP_MEMBER WHERE UserID = ?';
-
-            connection.query(insertQuery, [user.UserID, user.Password, user.Name, user.Email, user.Phone, user.Address], (err, result) => {
+            const getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTR(UserID, 2) AS UNSIGNED)) AS maxUserId FROM MEMBER WHERE UserID LIKE "U%"';
+            connection.query(getMaxUserIdQuery, (err, results) => {
                 if (err) return callback(err);
 
-                connection.query(deleteQuery, [user.UserID], (err, result) => {
+                const newUserId = `U${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
+
+                const insertQuery = 'INSERT INTO MEMBER (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified) VALUES (?, ?, ?, ?, ?, ?, 0, 1)';
+                const deleteQuery = 'DELETE FROM TEMP_MEMBER WHERE TempMemberID = ?';
+
+                connection.query(insertQuery, [newUserId, user.Password, user.Name, user.Email, user.Phone, user.Address], (err, result) => {
                     if (err) return callback(err);
-                    callback(null, { message: 'Email verified successfully' });
+
+                    connection.query(deleteQuery, [user.TempMemberID], (err, result) => {
+                        if (err) return callback(err);
+                        callback(null, { message: 'Email verified successfully', status: 200 });
+                    });
                 });
             });
         });
