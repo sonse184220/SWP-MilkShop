90822488c57e67424ec006ea0d79e1a29a4055ad
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-06-18 18:53:19 +0700

    Thêm redirect cho FE

--- src/backend/src/controllers/AuthController.js
+++ src/backend/src/controllers/AuthController.js
@@ -3,7 +3,6 @@ import { AuthService } from "../services/AuthService.js";
 const authService = new AuthService();
 
 export class AuthController {
-
     registerUser = (req, res) => {
         authService.registerUser(req.body, req, (err, result) => {
             if (err) return res.status(500).json({ error: err.message });
@@ -21,14 +20,11 @@ export class AuthController {
     verifyEmail = (req, res) => {
         authService.verifyEmail(req.query.token, (err, result) => {
             if (err) return res.status(500).json({ error: err.message });
-            res.status(result.status || 200).json(result);
-        });
-    };
-
-    completeProfile = (req, res) => {
-        authService.completeProfile(req, (err, result) => {
-            if (err) return res.status(500).json({ error: err.message });
-            res.status(200).json(result);
+            if (result.status === 200) {
+                res.redirect('http://localhost:3000/login-register');
+            } else {
+                res.status(result.status).json(result);
+            }
         });
     };
 }
