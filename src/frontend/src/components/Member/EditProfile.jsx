a43bd8d14103d91324ffabe0d15dbec9b7be5100
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-06-22 15:22:17 +0700

    Update EditProfile.jsx

--- src/frontend/src/components/Member/EditProfile.jsx
+++ src/frontend/src/components/Member/EditProfile.jsx
@@ -16,7 +16,7 @@ const EditProfile = ({ showLogin, userId }) => {
   const [isPasswordMatch, setIsPasswordMatch] = useState(true);
   const [SuccessMessage, setSuccessMessage] = useState("");
   const [ErrorMessage, setErrorMessage] = useState("");
-
+  const userId = JSON.parse(localStorage.getItem("userData")).UserID;
   useEffect(() => {
     const fetchUserData = async () => {
       try {
