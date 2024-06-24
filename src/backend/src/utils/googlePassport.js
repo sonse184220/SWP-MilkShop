6b14288d4c48bb90ac5c7e8b8070ea1117250370
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-06-24 22:56:56 +0700

    Update googlePassport.js

--- src/backend/src/utils/googlePassport.js
+++ src/backend/src/utils/googlePassport.js
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: 'http://localhost:4500/api/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     const email = profile.emails[0].value;
 
