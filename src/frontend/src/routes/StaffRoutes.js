7f5d9b38c073e974603f87765fa0cc5ce9e39e19
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-06-25 18:25:37 +0700

    Update StaffRoutes.js

--- src/frontend/src/routes/StaffRoutes.js
+++ src/frontend/src/routes/StaffRoutes.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import ProductManagement from '../components/Staff/ProductManagement.jsx';
+import OrderManagement from '../components/Staff/OrderManagement.jsx';
+import BlogManagement from '../components/Staff/BlogManagement.jsx';
 
 export function StaffRoutes() {
     return (
         <Routes>
             <Route path='ProductManagement' element={<ProductManagement />} />
+            <Route path='OrderManagement' element={<OrderManagement />} />
+            <Route path='BlogManagement' element={<BlogManagement />} />
         </Routes>
     )
 }
