0e5ac3c806913b399b4e56268445553f8b05dea2
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-06-26 23:06:03 +0700

    1

--- src/backend/dist/controllers/AuthController.js
+++ src/backend/dist/controllers/AuthController.js
@@ -3,162 +3,199 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.AuthController = void 0;
-var _EmailService = require("../services/EmailService.js");
-var _dbConnection = require("../utils/dbConnection.js");
+exports.loginUser = exports.completeProfile = void 0;
+Object.defineProperty(exports, "passport", {
+  enumerable: true,
+  get: function get() {
+    return _passport["default"];
+  }
+});
+exports.verifyEmail = exports.registerUser = void 0;
 var _bcryptjs = _interopRequireDefault(require("bcryptjs"));
 var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+var _passport = _interopRequireDefault(require("passport"));
+var _passportGoogleOauth = require("passport-google-oauth20");
+var _dbConnection = _interopRequireDefault(require("../utils/dbConnection.js"));
+var _emailService = require("../services/emailService.js");
 var _dotenv = _interopRequireDefault(require("dotenv"));
 var _excluded = ["Password"];
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _objectWithoutProperties(e, t) { if (null == e) return {}; var o, r, i = _objectWithoutPropertiesLoose(e, t); if (Object.getOwnPropertySymbols) { var n = Object.getOwnPropertySymbols(e); for (r = 0; r < n.length; r++) o = n[r], t.indexOf(o) >= 0 || {}.propertyIsEnumerable.call(e, o) && (i[o] = e[o]); } return i; }
 function _objectWithoutPropertiesLoose(r, e) { if (null == r) return {}; var t = {}; for (var n in r) if ({}.hasOwnProperty.call(r, n)) { if (e.indexOf(n) >= 0) continue; t[n] = r[n]; } return t; }
-function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
-function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 _dotenv["default"].config();
-var AuthController = exports.AuthController = /*#__PURE__*/_createClass(function AuthController() {
-  var _this = this;
-  _classCallCheck(this, AuthController);
-  _defineProperty(this, "registerUser", function (req, res) {
-    var _req$body = req.body,
-      UserID = _req$body.UserID,
-      Password = _req$body.Password,
-      Name = _req$body.Name,
-      Email = _req$body.Email,
-      Phone = _req$body.Phone,
-      Address = _req$body.Address;
-    var checkQuery = 'SELECT * FROM MEMBER WHERE UserID = ? OR Email = ? OR Phone = ?';
-    _dbConnection.connection.query(checkQuery, [UserID, Email, Phone], function (err, results) {
-      if (err) return res.status(500).json({
-        error: err.message
+_passport["default"].use(new _passportGoogleOauth.Strategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/api/auth/google/callback'
+}, function (accessToken, refreshToken, profile, done) {
+  var email = profile.emails[0].value;
+  var checkQuery = 'SELECT * FROM MEMBER WHERE Email = ?';
+  _dbConnection["default"].query(checkQuery, [email], function (err, results) {
+    if (err) return done(err);
+    if (results.length > 0) {
+      return done(null, results[0]);
+    }
+    var getMaxUserIdQuery = "SELECT MAX(CAST(SUBSTR(UserID, 3) AS UNSIGNED)) AS maxUserId FROM MEMBER WHERE UserID LIKE 'UG%'";
+    _dbConnection["default"].query(getMaxUserIdQuery, function (err, results) {
+      if (err) return done(err);
+      var newUserId = "UG".concat((results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0'));
+      var newUser = {
+        UserID: newUserId,
+        Name: profile.displayName || '',
+        Email: email,
+        Phone: '',
+        Address: '',
+        RewardPoints: 0,
+        Verified: 1
+      };
+      var query = 'INSERT INTO TEMP_MEMBER (UserID, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?)';
+      var token = _jsonwebtoken["default"].sign({
+        userId: newUserId
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
       });
-      if (results.length > 0) {
-        var existingUser = results[0];
-        var errorMessage = 'User already exists with ';
-        if (existingUser.UserID === UserID) {
-          errorMessage += 'this UserID';
-        } else if (existingUser.Email === Email) {
-          errorMessage += 'this Email';
-        } else if (existingUser.Phone === Phone) {
-          errorMessage += 'this Phone number';
-        }
-        return res.status(400).json({
-          message: errorMessage
-        });
+      _dbConnection["default"].query(query, [newUserId, newUser.Name, newUser.Email, newUser.Phone, newUser.Address, token], function (err, result) {
+        if (err) return done(err);
+        return done(null, newUser);
+      });
+    });
+  });
+}));
+_passport["default"].serializeUser(function (user, done) {
+  done(null, user.UserID);
+});
+_passport["default"].deserializeUser(function (id, done) {
+  var query = 'SELECT * FROM MEMBER WHERE UserID = ?';
+  _dbConnection["default"].query(query, [id], function (err, results) {
+    if (err) return done(err);
+    done(null, results[0]);
+  });
+});
+var registerUser = exports.registerUser = function registerUser(userData, req, callback) {
+  var Password = userData.Password,
+    Name = userData.Name,
+    Email = userData.Email,
+    Phone = userData.Phone,
+    Address = userData.Address;
+  var checkQuery = 'SELECT * FROM MEMBER WHERE Email = ? OR Phone = ?';
+  _dbConnection["default"].query(checkQuery, [Email, Phone], function (err, results) {
+    if (err) return callback(err);
+    if (results.length > 0) {
+      var existingUser = results[0];
+      var errorMessage = 'User already exists with ';
+      if (existingUser.Email === Email) {
+        errorMessage += 'this Email';
+      } else if (existingUser.Phone === Phone) {
+        errorMessage += 'this Phone number';
       }
-      _bcryptjs["default"].hash(Password, 10, function (err, hashedPassword) {
-        if (err) return res.status(500).json({
-          error: err.message
-        });
+      return callback(null, {
+        message: errorMessage,
+        status: 400
+      });
+    }
+    _bcryptjs["default"].hash(Password, 10, function (err, hashedPassword) {
+      if (err) return callback(err);
+      var getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTR(UserID, 2) AS UNSIGNED)) AS maxUserId FROM MEMBER WHERE UserID LIKE "U%"';
+      _dbConnection["default"].query(getMaxUserIdQuery, function (err, results) {
+        if (err) return callback(err);
+        var newUserId = "U".concat((results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0'));
         var token = _jsonwebtoken["default"].sign({
-          userId: UserID
+          userId: newUserId
         }, process.env.JWT_SECRET, {
           expiresIn: '1h'
         });
         var query = 'INSERT INTO TEMP_MEMBER (UserID, Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?, ?)';
-        _dbConnection.connection.query(query, [UserID, hashedPassword, Name, Email, Phone, Address, token], function (err, result) {
-          if (err) return res.status(500).json({
-            error: err.message
-          });
-          _this.emailService.sendVerificationEmail(Email, token, UserID, Phone, req).then(function () {
+        _dbConnection["default"].query(query, [newUserId, hashedPassword, Name, Email, Phone, Address, token], function (err, result) {
+          if (err) return callback(err);
+          (0, _emailService.sendVerificationEmail)(Email, token, newUserId, Phone, req).then(function () {
             console.log('Verification email sent to:', Email);
-            res.status(201).json({
+            callback(null, {
               message: 'User registered successfully. Verification email sent.'
             });
           })["catch"](function (error) {
             console.error('Error sending verification email:', error);
-            res.status(500).json({
-              error: error.message
-            });
+            callback(error);
           });
         });
       });
     });
   });
-  _defineProperty(this, "loginUser", function (req, res) {
-    var _req$body2 = req.body,
-      UserID = _req$body2.UserID,
-      Password = _req$body2.Password;
-    var query = 'SELECT * FROM MEMBER WHERE UserID = ?';
-    _dbConnection.connection.query(query, [UserID], function (err, results) {
-      if (err) return res.status(500).json({
-        error: err.message
+};
+var loginUser = exports.loginUser = function loginUser(userData, callback) {
+  var identifier = userData.identifier,
+    Password = userData.Password;
+  var query = 'SELECT * FROM MEMBER WHERE Email = ? OR Phone = ?';
+  _dbConnection["default"].query(query, [identifier, identifier], function (err, results) {
+    if (err) return callback(err);
+    if (results.length === 0) {
+      return callback(null, {
+        message: 'Invalid credentials',
+        status: 401
       });
-      if (results.length === 0) {
-        return res.status(401).json({
-          message: 'Invalid UserID'
+    }
+    var user = results[0];
+    _bcryptjs["default"].compare(Password, user.Password, function (err, isMatch) {
+      if (err) return callback(err);
+      if (!isMatch) {
+        return callback(null, {
+          message: 'Invalid credentials',
+          status: 401
         });
       }
-      var user = results[0];
-      _bcryptjs["default"].compare(Password, user.Password, function (err, isMatch) {
-        if (err) return res.status(500).json({
-          error: err.message
-        });
-        if (!isMatch) {
-          return res.status(401).json({
-            message: 'Invalid Password'
-          });
-        }
-        var token = _jsonwebtoken["default"].sign({
-          userId: user.UserID
-        }, process.env.JWT_SECRET, {
-          expiresIn: '1h'
-        });
-        var Password = user.Password,
-          userWithoutPassword = _objectWithoutProperties(user, _excluded);
-        res.status(200).json({
-          message: 'Login successful',
-          token: token,
-          user: userWithoutPassword
-        });
+      var token = _jsonwebtoken["default"].sign({
+        userId: user.UserID
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+      });
+      var Password = user.Password,
+        userWithoutPassword = _objectWithoutProperties(user, _excluded);
+      callback(null, {
+        message: 'Login successful',
+        token: token,
+        user: userWithoutPassword
       });
     });
   });
-  _defineProperty(this, "verifyEmail", function (req, res) {
-    var token = req.query.token;
-    if (!token) {
-      return res.status(400).json({
-        message: 'Verification token is required'
+};
+var verifyEmail = exports.verifyEmail = function verifyEmail(token, callback) {
+  try {
+    var decoded = _jsonwebtoken["default"].verify(token, process.env.JWT_SECRET);
+    var userId = decoded.userId;
+    var selectQuery = 'SELECT * FROM TEMP_MEMBER WHERE Token = ?';
+    _dbConnection["default"].query(selectQuery, [token], function (err, results) {
+      if (err) return callback(err);
+      if (results.length === 0) return callback(null, {
+        message: 'Invalid or expired token',
+        status: 400
       });
-    }
-    try {
-      var decoded = _jsonwebtoken["default"].verify(token, process.env.JWT_SECRET);
-      var userId = decoded.userId;
-      var selectQuery = 'SELECT * FROM TEMP_MEMBER WHERE Token = ?';
-      _dbConnection.connection.query(selectQuery, [token], function (err, results) {
-        if (err) return res.status(500).json({
-          error: err.message
-        });
-        if (results.length === 0) return res.status(400).json({
-          message: 'Invalid or expired token'
-        });
-        var user = results[0];
-        var insertQuery = 'INSERT INTO MEMBER (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified) VALUES (?, ?, ?, ?, ?, ?, 0, 1)';
-        var deleteQuery = 'DELETE FROM TEMP_MEMBER WHERE UserID = ?';
-        _dbConnection.connection.query(insertQuery, [user.UserID, user.Password, user.Name, user.Email, user.Phone, user.Address], function (err, result) {
-          if (err) return res.status(500).json({
-            error: err.message
-          });
-          _dbConnection.connection.query(deleteQuery, [user.UserID], function (err, result) {
-            if (err) return res.status(500).json({
-              error: err.message
-            });
-            res.status(200).json({
-              message: 'Email verified successfully'
-            });
+      var user = results[0];
+      var insertQuery = 'INSERT INTO MEMBER (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified) VALUES (?, ?, ?, ?, ?, ?, 0, 1)';
+      var deleteQuery = 'DELETE FROM TEMP_MEMBER WHERE UserID = ?';
+      _dbConnection["default"].query(insertQuery, [user.UserID, user.Password, user.Name, user.Email, user.Phone, user.Address], function (err, result) {
+        if (err) return callback(err);
+        _dbConnection["default"].query(deleteQuery, [user.UserID], function (err, result) {
+          if (err) return callback(err);
+          callback(null, {
+            message: 'Email verified successfully'
           });
         });
       });
-    } catch (err) {
-      res.status(500).json({
-        error: err.message
-      });
-    }
+    });
+  } catch (err) {
+    callback(err);
+  }
+};
+var completeProfile = exports.completeProfile = function completeProfile(req, callback) {
+  var _req$body = req.body,
+    userId = _req$body.userId,
+    name = _req$body.name,
+    phone = _req$body.phone,
+    address = _req$body.address;
+  var query = 'UPDATE MEMBER SET Name = ?, Phone = ?, Address = ? WHERE UserID = ?';
+  _dbConnection["default"].query(query, [name, phone, address, userId], function (err, result) {
+    if (err) return callback(err);
+    callback(null, {
+      message: 'Profile completed successfully'
+    });
   });
-  this.emailService = new _EmailService.EmailService();
-});
+};
--- src/backend/dist/services/AuthService.js
+++ src/backend/dist/services/AuthService.js
@@ -1,140 +1,42 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.AuthService = void 0;
-var _dbConnection = require("../utils/dbConnection.js");
-var _EmailService = require("./EmailService.js");
-var _bcryptjs = _interopRequireDefault(require("bcryptjs"));
-var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
-var _dotenv = _interopRequireDefault(require("dotenv"));
-var _excluded = ["Password"];
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _objectWithoutProperties(e, t) { if (null == e) return {}; var o, r, i = _objectWithoutPropertiesLoose(e, t); if (Object.getOwnPropertySymbols) { var n = Object.getOwnPropertySymbols(e); for (r = 0; r < n.length; r++) o = n[r], t.indexOf(o) >= 0 || {}.propertyIsEnumerable.call(e, o) && (i[o] = e[o]); } return i; }
-function _objectWithoutPropertiesLoose(r, e) { if (null == r) return {}; var t = {}; for (var n in r) if ({}.hasOwnProperty.call(r, n)) { if (e.indexOf(n) >= 0) continue; t[n] = r[n]; } return t; }
-function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
-function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-_dotenv["default"].config();
-var AuthService = exports.AuthService = /*#__PURE__*/_createClass(function AuthService() {
-  var _this = this;
-  _classCallCheck(this, AuthService);
-  _defineProperty(this, "registerUser", function (userData, req, callback) {
-    var UserID = userData.UserID,
-      Password = userData.Password,
-      Name = userData.Name,
-      Email = userData.Email,
-      Phone = userData.Phone,
-      Address = userData.Address;
-    var checkQuery = 'SELECT * FROM MEMBER WHERE UserID = ? OR Email = ? OR Phone = ?';
-    _dbConnection.connection.query(checkQuery, [UserID, Email, Phone], function (err, results) {
-      if (err) return callback(err);
-      if (results.length > 0) {
-        var existingUser = results[0];
-        var errorMessage = 'User already exists with ';
-        if (existingUser.UserID === UserID) {
-          errorMessage += 'this UserID';
-        } else if (existingUser.Email === Email) {
-          errorMessage += 'this Email';
-        } else if (existingUser.Phone === Phone) {
-          errorMessage += 'this Phone number';
-        }
-        return callback(null, {
-          message: errorMessage,
-          status: 400
-        });
-      }
-      _bcryptjs["default"].hash(Password, 10, function (err, hashedPassword) {
-        if (err) return callback(err);
-        var token = _jsonwebtoken["default"].sign({
-          userId: UserID
-        }, process.env.JWT_SECRET, {
-          expiresIn: '1h'
-        });
-        var query = 'INSERT INTO TEMP_MEMBER (UserID, Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?, ?)';
-        _dbConnection.connection.query(query, [UserID, hashedPassword, Name, Email, Phone, Address, token], function (err, result) {
-          if (err) return callback(err);
-          _this.emailService.sendVerificationEmail(Email, token, UserID, Phone, req).then(function () {
-            console.log('Verification email sent to:', Email);
-            callback(null, {
-              message: 'User registered successfully. Verification email sent.'
-            });
-          })["catch"](function (error) {
-            console.error('Error sending verification email:', error);
-            callback(error);
-          });
-        });
-      });
+exports.verifyEmail = exports.registerUser = exports.loginUser = exports.completeProfile = void 0;
+var authController = _interopRequireWildcard(require("../controllers/AuthController.js"));
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+var registerUser = exports.registerUser = function registerUser(req, res) {
+  authController.registerUser(req.body, req, function (err, result) {
+    if (err) return res.status(500).json({
+      error: err.message
     });
+    res.status(result.status || 201).json(result);
   });
-  _defineProperty(this, "loginUser", function (userData, callback) {
-    var UserID = userData.UserID,
-      Password = userData.Password;
-    var query = 'SELECT * FROM MEMBER WHERE UserID = ?';
-    _dbConnection.connection.query(query, [UserID], function (err, results) {
-      if (err) return callback(err);
-      if (results.length === 0) {
-        return callback(null, {
-          message: 'Invalid UserID',
-          status: 401
-        });
-      }
-      var user = results[0];
-      _bcryptjs["default"].compare(Password, user.Password, function (err, isMatch) {
-        if (err) return callback(err);
-        if (!isMatch) {
-          return callback(null, {
-            message: 'Invalid Password',
-            status: 401
-          });
-        }
-        var token = _jsonwebtoken["default"].sign({
-          userId: user.UserID
-        }, process.env.JWT_SECRET, {
-          expiresIn: '1h'
-        });
-        var Password = user.Password,
-          userWithoutPassword = _objectWithoutProperties(user, _excluded);
-        callback(null, {
-          message: 'Login successful',
-          token: token,
-          user: userWithoutPassword
-        });
-      });
+};
+var loginUser = exports.loginUser = function loginUser(req, res) {
+  authController.loginUser(req.body, function (err, result) {
+    if (err) return res.status(500).json({
+      error: err.message
     });
+    res.status(result.status || 200).json(result);
   });
-  _defineProperty(this, "verifyEmail", function (token, callback) {
-    try {
-      var decoded = _jsonwebtoken["default"].verify(token, process.env.JWT_SECRET);
-      var userId = decoded.userId;
-      var selectQuery = 'SELECT * FROM TEMP_MEMBER WHERE Token = ?';
-      _dbConnection.connection.query(selectQuery, [token], function (err, results) {
-        if (err) return callback(err);
-        if (results.length === 0) return callback(null, {
-          message: 'Invalid or expired token',
-          status: 400
-        });
-        var user = results[0];
-        var insertQuery = 'INSERT INTO MEMBER (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified) VALUES (?, ?, ?, ?, ?, ?, 0, 1)';
-        var deleteQuery = 'DELETE FROM TEMP_MEMBER WHERE UserID = ?';
-        _dbConnection.connection.query(insertQuery, [user.UserID, user.Password, user.Name, user.Email, user.Phone, user.Address], function (err, result) {
-          if (err) return callback(err);
-          _dbConnection.connection.query(deleteQuery, [user.UserID], function (err, result) {
-            if (err) return callback(err);
-            callback(null, {
-              message: 'Email verified successfully'
-            });
-          });
-        });
-      });
-    } catch (err) {
-      callback(err);
-    }
+};
+var verifyEmail = exports.verifyEmail = function verifyEmail(req, res) {
+  authController.verifyEmail(req.query.token, function (err, result) {
+    if (err) return res.status(500).json({
+      error: err.message
+    });
+    res.status(result.status || 200).json(result);
   });
-  this.emailService = new _EmailService.EmailService();
-});
+};
+var completeProfile = exports.completeProfile = function completeProfile(req, res) {
+  authController.completeProfile(req, function (err, result) {
+    if (err) return res.status(500).json({
+      error: err.message
+    });
+    res.status(200).json(result);
+  });
+};
