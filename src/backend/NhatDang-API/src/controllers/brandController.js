1df0139ed4869ce3ab8dcf9b2a9e3d27c3672dae
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-06-28 12:32:24 +0700

    Làm chung gòi nên dẹp thư mục demo

--- src/backend/NhatDang-API/src/controllers/brandController.js
+++
@@ -1,8 +0,0 @@
-import * as brandService from '../services/brandService.js';
-
-export const getAllBrands = (req, res) => {
-    brandService.getAllBrands((err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.status(200).json(results);
-    });
-};
