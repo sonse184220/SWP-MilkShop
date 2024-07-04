39e1c2be2bfcbc9f75f77d5b1e5872b729d9ded8
LQVinh11022004 <vinhlqse184287@fpt.edu.vn>
2024-07-04 17:31:21 +0700

    chinh sua

--- src/frontend/src/routes/AdminRoutes.js
+++ src/frontend/src/routes/AdminRoutes.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import StaffManagement from '../components/Admin/StaffManagement';
 import MemberManagement from '../components/Admin/MemberManagement';
 import Dashboard from '../components/Admin/Dashboard';
+import AdminProfile from '../components/Admin/AdminProfile';
 
 
 export function AdminRoutes() {
@@ -10,6 +11,7 @@ export function AdminRoutes() {
             <Route path='StaffManagement' element={<StaffManagement/>} />
             <Route path='MemberManagement' element={<MemberManagement/>} />
             <Route path='Dashboard' element={<Dashboard/>} />
+            <Route path='AdminProfile' element={<AdminProfile/>} />
 
         </Routes>
     )
