77fe7844f50f519df82592b8860f7d816d18e6f1
PTPhong-PPL <phongptse183388@fpt.edu.vn>
2024-07-04 17:55:10 +0700

    Update API get all blogs

--- src/backend/src/controllers/BlogController.js
+++ src/backend/src/controllers/BlogController.js
@@ -22,8 +22,31 @@ export class BlogController {
     };
 
     async getAllBlogs(req, res) {
-        const blogs = await blogService.getAllBlogs();
-        res.status(200).send(blogs);
+        const limit = parseInt(req.query.limit);
+        const page = parseInt(req.query.page);
+        const sort = req.query.sort;
+        const offset = (page - 1) * limit;
+
+        let sortBy;
+        switch (sort) {
+            case "newest":
+                sortBy = "updated DESC";
+                break;
+            case "oldest":
+                sortBy = "updated ASC";
+                break;
+            default:
+                sortBy = "updated DESC";
+        }
+
+        const blogs = await blogService.getAllBlogs(limit, sortBy, offset);
+        const total = await blogService.getTotalBlogs();
+        res.status(200).send({
+            total: total,
+            page: page,
+            totalPages: Math.ceil(total / limit),
+            data: blogs,
+        });
     };
 
     async searchBlogs(req, res) {
