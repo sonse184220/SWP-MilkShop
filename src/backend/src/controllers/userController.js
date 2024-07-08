c8ac658f8782d01b82d18499d2504a895441a292
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-07-09 00:06:17 +0700

    lam lai user callback -> async/await

--- src/backend/src/controllers/userController.js
+++ src/backend/src/controllers/userController.js
@@ -3,37 +3,41 @@ import { UserService } from '../services/userService.js';
 const userService = new UserService();
 
 export class UserController {
-    getUserInfo = (req, res) => {
-        const userId = req.params.userId;
-        userService.getUserInfo(userId, (err, results) => {
-            if (err) return res.status(500).json({ error: err.message });
-            if (results.length === 0) return res.status(404).json({ message: 'User not found' });
-            res.status(200).json(results[0]);
-        });
-    };
-
-    updateUserInfo = (req, res) => {
-        const userId = req.params.userId;
-        const newUserData = {
-            ...req.body,
-            ProfilePicture: req.file ? req.file.buffer : null,
-        };
-
-        userService.updateUserInfo(userId, newUserData, (err, result) => {
-            if (err) {
-                return res.status(500).json({ error: err.message });
+    async getUserInfo(req, res) {
+        try {
+            const userId = req.params.userId;
+            const userInfo = await userService.getUserInfo(userId);
+            if (!userInfo) {
+                return res.status(404).json({ message: 'User not found' });
             }
-            res.status(200).json(result);
-        });
-    };
+            res.status(200).json(userInfo);
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
 
-    changePassword = (req, res) => {
-        const userId = req.user.userId;
-        const { oldPassword, newPassword } = req.body;
-
-        userService.changePassword(userId, oldPassword, newPassword, (err, result) => {
-            if (err) return res.status(500).json({ error: err.message });
+    async updateUserInfo(req, res) {
+        try {
+            const userId = req.params.userId;
+            const newUserData = {
+                ...req.body,
+                ProfilePicture: req.file ? req.file.buffer : null,
+            };
+            const result = await userService.updateUserInfo(userId, newUserData);
             res.status(200).json(result);
-        });
-    };
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
+
+    async changePassword(req, res) {
+        try {
+            const userId = req.user.userId;
+            const { oldPassword, newPassword } = req.body;
+            const result = await userService.changePassword(userId, oldPassword, newPassword);
+            res.status(200).json(result);
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
 }
