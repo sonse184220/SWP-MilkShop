008d20bb674894cfe4041dfe71ae54acd87ee159
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-07-08 10:00:21 +0700

    Update adminRoutes.js

--- src/backend/src/routes/adminRoutes.js
+++ src/backend/src/routes/adminRoutes.js
@@ -10,5 +10,6 @@ const adminController = new AdminController();
 router.post('/disable-account', passport.authenticate('jwt', { session: false }), isAdmin, adminController.disableAccount);
 router.post('/enable-account', passport.authenticate('jwt', { session: false }), isAdmin, adminController.enableAccount);
 router.get('/total-revenue', passport.authenticate('jwt', { session: false }), isAdmin, adminController.getTotalRevenue);
+router.get('/accounts', passport.authenticate('jwt', { session: false }), isAdmin, adminController.getAllAccounts);
 
 export { router as adminRoutes };
