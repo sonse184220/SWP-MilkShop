0e113b32bb069bb7e31431b2ba0c878f2336cd53
nhatdlmse184630 <nhatdlmse184630@fpt.edu.vn>
2024-07-09 11:37:27 +0700

    Update AuthService.js

--- src/backend/src/services/AuthService.js
+++ src/backend/src/services/AuthService.js
@@ -1,8 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
-import { connection } from '../utils/dbConnection.js';
+import { poolConnect } from '../utils/dbConnection.js';
 import { EmailService } from './EmailService.js';
 
 dotenv.config();
@@ -12,176 +11,128 @@ export class AuthService {
         this.emailService = new EmailService();
     }
 
-    registerUser = (userData, req, callback) => {
+    async registerUser(userData, req) {
         const { Password, Name, Email, Phone, Address } = userData;
 
-        const checkQuery = 'SELECT * FROM `user` WHERE Email = ?';
-        connection.query(checkQuery, [Email], (err, results) => {
-            if (err) return callback(err);
+        const checkQuery = 'Select * from `user` WHERE Email = ?';
+        const [results] = await poolConnect.query(checkQuery, [Email]);
+        if (results.length > 0) {
+            throw new Error('User already exists with this Email');
+        }
 
-            if (results.length > 0) {
-                return callback(null, { message: 'User already exists with this Email', status: 400 });
-            }
+        const tempCheckQuery = 'Select * from `TEMP_USER` WHERE Email = ?';
+        const [tempResults] = await poolConnect.query(tempCheckQuery, [Email]);
+        if (tempResults.length > 0) {
+            throw new Error('A verification email has already been sent to this Email.');
+        }
 
-            const tempCheckQuery = 'SELECT * FROM `TEMP_USER` WHERE Email = ?';
-            connection.query(tempCheckQuery, [Email], (err, results) => {
-                if (err) return callback(err);
+        const hashedPassword = await bcrypt.hash(Password, 10);
 
-                if (results.length > 0) {
-                    return callback(null, { message: 'A verification email has already been sent to this Email.', status: 400 });
-                }
+        const token = jwt.sign({ email: Email }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        const query = 'Insert INTO `TEMP_USER` (Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?)';
 
-                bcrypt.hash(Password, 10, (err, hashedPassword) => {
-                    if (err) return callback(err);
+        const [result] = await poolConnect.query(query, [hashedPassword, Name, Email, Phone, Address, token]);
 
-                    const token = jwt.sign({ email: Email }, process.env.JWT_SECRET, { expiresIn: '24h' });
-                    const query = 'INSERT INTO `TEMP_USER` (Password, Name, Email, Phone, Address, Token) VALUES (?, ?, ?, ?, ?, ?)';
+        await this.emailService.sendVerificationEmail(Email, token, result.insertId, Phone, req);
+        return { message: 'User registered successfully. Verification email sent.', token };
+    }
 
-                    connection.query(query, [hashedPassword, Name, Email, Phone, Address, token], (err, result) => {
-                        if (err) return callback(err);
-
-                        this.emailService.sendVerificationEmail(Email, token, result.insertId, Phone, req)
-                            .then(() => {
-                                console.log('Verification email sent to:', Email);
-                                callback(null, { message: 'User registered successfully. Verification email sent.', token });
-                            })
-                            .catch(error => {
-                                console.error('Error sending verification email:', error);
-                                callback(error);
-                            });
-                    });
-                });
-            });
-        });
-    };
-
-    loginUser = (userData, callback) => {
+    async loginUser(userData) {
         const { identifier, Password } = userData;
 
         const query = 'SELECT * FROM `user` WHERE Email = ? OR Phone = ?';
-        connection.query(query, [identifier, identifier], (err, results) => {
-            if (err) return callback(err);
-
-            if (results.length === 0) {
-                return callback(null, { message: 'Invalid credentials', status: 401 });
-            }
-
-            const user = results[0];
-
-            if (user.activeStatus === 'inactive') {
-                return callback(null, { message: 'Account is inactive. Please contact support.', status: 403 });
-            }
-
-            bcrypt.compare(Password, user.Password, (err, isMatch) => {
-                if (err) return callback(err);
-
-                if (!isMatch) {
-                    return callback(null, { message: 'Invalid credentials', status: 401 });
-                }
-
-                const token = jwt.sign({
-                    userId: user.UserID,
-                    isAdmin: user.isAdmin,
-                    isStaff: user.isStaff
-                }, process.env.JWT_SECRET, { expiresIn: '24h' });
-                const { Password, ...userWithoutPassword } = user;
-                callback(null, { message: 'Login successful', token, user: userWithoutPassword });
-            });
-        });
-    };
-
-    verifyEmail = (token, callback) => {
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const email = decoded.email;
-
-            const selectQuery = 'SELECT * FROM `TEMP_USER` WHERE Token = ?';
-            connection.query(selectQuery, [token], (err, results) => {
-                if (err) return callback(err);
-                if (results.length === 0) return callback(null, { message: 'Invalid or expired token', status: 400 });
-
-                const user = results[0];
-                const getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) AS maxUserId FROM `user` WHERE UserID LIKE "U%"';
-                connection.query(getMaxUserIdQuery, (err, results) => {
-                    if (err) return callback(err);
-
-                    const newUserId = `U${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
-
-                    const insertQuery = 'INSERT INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 0, 0, "active")';
-                    const deleteQuery = 'DELETE FROM `TEMP_USER` WHERE TempUserID = ?';
-
-                    connection.query(insertQuery, [newUserId, user.Password, user.Name, user.Email, user.Phone, user.Address], (err, result) => {
-                        if (err) return callback(err);
-
-                        connection.query(deleteQuery, [user.TempUserID], (err, result) => {
-                            if (err) return callback(err);
-                            callback(null, { message: 'Email verified successfully', status: 200 });
-                        });
-                    });
-                });
-            });
-        } catch (err) {
-            callback(err);
+        const [results] = await poolConnect.query(query, [identifier, identifier]);
+        if (results.length === 0) {
+            throw new Error('Invalid credentials');
         }
-    };
 
-    registerAdmin = (userData, callback) => {
+        const user = results[0];
+
+        if (user.activeStatus === 'inactive') {
+            throw new Error('Account is inactive. Please contact support.');
+        }
+
+        const isMatch = await bcrypt.compare(Password, user.Password);
+        if (!isMatch) {
+            throw new Error('Invalid credentials');
+        }
+
+        const token = jwt.sign({
+            userId: user.UserID,
+            isAdmin: user.isAdmin,
+            isStaff: user.isStaff
+        }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+        const { Password: userPassword, ...userWithoutPassword } = user;
+        return { message: 'Login successful', token, user: userWithoutPassword };
+    }
+
+    async verifyEmail(token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const email = decoded.email;
+
+        const selectQuery = 'Select * FROM `TEMP_USER` WHERE Token = ?';
+        const [results] = await poolConnect.query(selectQuery, [token]);
+        if (results.length === 0) {
+            throw new Error('Invalid or expired token');
+        }
+
+        const user = results[0];
+        const getMaxUserIdQuery = 'Select MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) as maxUserId FROM `user` WHERE UserID LIKE "U%"';
+        const [maxUserIdResults] = await poolConnect.query(getMaxUserIdQuery);
+
+        const newUserId = `U${(maxUserIdResults[0].maxUserId ? maxUserIdResults[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
+
+        const insertQuery = 'Insert INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 0, 0, "active")';
+        const deleteQuery = 'Delete FROM `TEMP_USER` WHERE TempUserID = ?';
+
+        await poolConnect.query(insertQuery, [newUserId, user.Password, user.Name, user.Email, user.Phone, user.Address]);
+        await poolConnect.query(deleteQuery, [user.TempUserID]);
+
+        return { message: 'Email verified successfully', status: 200 };
+    }
+
+    async registerAdmin(userData) {
         const { Password, Name, Email, Phone, Address } = userData;
 
-        const checkQuery = 'SELECT * FROM `user` WHERE Email = ?';
-        connection.query(checkQuery, [Email], (err, results) => {
-            if (err) return callback(err);
+        const checkQuery = 'Select * From `user` WHERE Email = ?';
+        const [results] = await poolConnect.query(checkQuery, [Email]);
+        if (results.length > 0) {
+            throw new Error('User already exists with this Email');
+        }
 
-            if (results.length > 0) {
-                return callback(null, { message: 'User already exists with this Email', status: 400 });
-            }
+        const hashedPassword = await bcrypt.hash(Password, 10);
 
-            bcrypt.hash(Password, 10, (err, hashedPassword) => {
-                if (err) return callback(err);
+        const getMaxUserIdQuery = 'Select MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) as maxUserId FROM `user` WHERE UserID LIKE "U%"';
+        const [maxUserIdResults] = await poolConnect.query(getMaxUserIdQuery);
 
-                const getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) AS maxUserId FROM `user` WHERE UserID LIKE "U%"';
-                connection.query(getMaxUserIdQuery, (err, results) => {
-                    if (err) return callback(err);
+        const newUserId = `U${(maxUserIdResults[0].maxUserId ? maxUserIdResults[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
 
-                    const newUserId = `U${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
+        const query = 'Insert INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 1, 0, "active")';
+        await poolConnect.query(query, [newUserId, hashedPassword, Name, Email, Phone, Address]);
 
-                    const query = 'INSERT INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 1, 0, "active")';
-                    connection.query(query, [newUserId, hashedPassword, Name, Email, Phone, Address], (err, result) => {
-                        if (err) return callback(err);
-                        callback(null, { message: 'Admin registered successfully', status: 201 });
-                    });
-                });
-            });
-        });
-    };
+        return { message: 'Admin registered successfully', status: 201 };
+    }
 
-    createStaff = (userData, callback) => {
+    async createStaff(userData) {
         const { Password, Name, Email, Phone, Address } = userData;
 
-        const checkQuery = 'SELECT * FROM `user` WHERE Email = ?';
-        connection.query(checkQuery, [Email], (err, results) => {
-            if (err) return callback(err);
+        const checkQuery = 'Select * FROM `user` WHERE Email = ?';
+        const [results] = await poolConnect.query(checkQuery, [Email]);
+        if (results.length > 0) {
+            throw new Error('User already exists with this Email');
+        }
 
-            if (results.length > 0) {
-                return callback(null, { message: 'User already exists with this Email', status: 400 });
-            }
+        const hashedPassword = await bcrypt.hash(Password, 10);
 
-            bcrypt.hash(Password, 10, (err, hashedPassword) => {
-                if (err) return callback(err);
+        const getMaxUserIdQuery = 'Select MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) AS maxUserId FROM `user` WHERE UserID LIKE "U%"';
+        const [maxUserIdResults] = await poolConnect.query(getMaxUserIdQuery);
 
-                const getMaxUserIdQuery = 'SELECT MAX(CAST(SUBSTRING(UserID, 2) AS UNSIGNED)) AS maxUserId FROM `user` WHERE UserID LIKE "U%"';
-                connection.query(getMaxUserIdQuery, (err, results) => {
-                    if (err) return callback(err);
+        const newUserId = `U${(maxUserIdResults[0].maxUserId ? maxUserIdResults[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
 
-                    const newUserId = `U${(results[0].maxUserId ? results[0].maxUserId + 1 : 1).toString().padStart(3, '0')}`;
+        const query = 'Insert INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 0, 1, "active")';
+        await poolConnect.query(query, [newUserId, hashedPassword, Name, Email, Phone, Address]);
 
-                    const query = 'INSERT INTO `user` (UserID, Password, Name, Email, Phone, Address, RewardPoints, Verified, isAdmin, isStaff, activeStatus) VALUES (?, ?, ?, ?, ?, ?, 0, 1, 0, 1, "active")';
-                    connection.query(query, [newUserId, hashedPassword, Name, Email, Phone, Address], (err, result) => {
-                        if (err) return callback(err);
-                        callback(null, { message: 'Staff created successfully', status: 201 });
-                    });
-                });
-            });
-        });
-    };
+        return { message: 'Staff created successfully', status: 201 };
+    }
 }
